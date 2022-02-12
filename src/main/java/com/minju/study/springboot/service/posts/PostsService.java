4e96b786d7d9c42cee1f3d3e687b2bd69430fb33
dooroojoo <dooroojoo@kakao.com>
2022-02-12 19:06:53 +0900

    update 기능 생성 및 테스트(update 테스트 오류 확인중)

--- src/main/java/com/minju/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/minju/study/springboot/service/posts/PostsService.java
@@ -1,11 +1,14 @@
 package com.minju.study.springboot.service.posts;
 
+import com.minju.study.springboot.domain.posts.Posts;
 import com.minju.study.springboot.domain.posts.PostsRepository;
+import com.minju.study.springboot.web.dto.PostsResponseDto;
 import com.minju.study.springboot.web.dto.PostsSaveRequestDto;
+import com.minju.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
