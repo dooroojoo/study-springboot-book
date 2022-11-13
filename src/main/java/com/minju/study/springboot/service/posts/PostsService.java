fe6117aa5b4f7b01bb7326c8ce4097001e0fe200
dooroojoo <dooroojoo@kakao.com>
2022-11-13 20:46:48 +0900

    게시글 등록, 조회 화면 만들기

--- src/main/java/com/minju/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/minju/study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.minju.study.springboot.service.posts;
 
 import com.minju.study.springboot.domain.posts.Posts;
 import com.minju.study.springboot.domain.posts.PostsRepository;
+import com.minju.study.springboot.web.dto.PostsListResponseDto;
 import com.minju.study.springboot.web.dto.PostsResponseDto;
 import com.minju.study.springboot.web.dto.PostsSaveRequestDto;
 import com.minju.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -31,5 +35,9 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
